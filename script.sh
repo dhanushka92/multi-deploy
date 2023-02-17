019aaebb3de5ad89582a1b8efd4350f4330b6042
dhanushka92 <Abdulla.Ansar@al-ghurair.com>
2023-02-17 11:41:55 +0400

    family cli

--- script.sh
+++ script.sh
@@ -2,5 +2,5 @@
 ROLE_ARN="$(aws ecs describe-task-definition --task-definition agcars-frontend --region me-south-1 | jq .taskDefinition.executionRoleArn)"
 echo "ROLE_ARN= " $ROLE_ARN
 
-FAMILY="$(aws ecs describe-task-definition --task-definition agcars-frontend --region me-south-1 | jq .taskDefinition.FAMILY)"
+FAMILY="$(aws ecs describe-task-definition --task-definition agcars-frontend --region me-south-1 | jq .taskDefinition.family)"
 echo "FAMILY= " $FAMILY
